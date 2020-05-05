af7bd67df35b09bea71f709d6e1148532b9c607a
Saeed Rahman <saeed349>
2020-05-04 22:28:18 -0400

    update : added new rest api container for etl - alpaca for now

--- Storage/etl/app.py
+++ Storage/etl/app.py
@@ -1,10 +1,41 @@
 import os
+import subprocess
 
 from flask import Flask, request
 from flask_restful import Resource, Api
+import db_pack.alpaca.alpaca_daily as alpaca_daily
+import db_pack.alpaca.alpaca_symbol_loader as alpaca_symbol_loader
 
 app = Flask(__name__)
 api = Api(app)
+
+class load_symbols(Resource):
+    def get(self,vendor):
+        alpaca_symbol_loader.main()
+        return {'Success':'Symbols Loaded'}
+
+
+class load_daily_data(Resource):
+    def get(self,vendor):
+        alpaca_daily.main()
+        return {'Success':'Daily Data Loaded'}
+class load_indicator_data(Resource):
+    def get(self,vendor):
+        cmd_str="""python q_pack/q_run/run_BT.py --todate='2020-03-31'"""
+        subprocess.run(cmd_str, shell=True)
+        return {'Success':'Indicator Data Loaded'}
+
+class test(Resource):
+    def get(self,vendor):
+        cmd_str="""echo poda'"""
+        subprocess.run(cmd_str, shell=True)
+        return {'Success':'Test'}
+
+
+api.add_resource(load_symbols,'/load_symbols/<string:vendor>')
+api.add_resource(load_daily_data,'/load_daily_data/<string:vendor>')
+api.add_resource(load_indicator_data,'/load_indicator_data/<string:vendor>')
+api.add_resource(test,'/test/<string:vendor>')
 
 class HelloWorld(Resource):
     def get(self):
@@ -14,12 +45,12 @@ class HelloWorld(Resource):
         some_json=request.get_json()
         return{'you sent':some_json}, 201
 
-class Multi(Resource):
-    def get(self,num):
-        return {'result':num*10}
+# class Multi(Resource):
+#     def get(self,num):
+#         return {'result':num*10}
 
 api.add_resource(HelloWorld,'/')
-api.add_resource(Multi,'/mulit/<int:num>')
+# api.add_resource(Multi,'/mulit/<int:num>')
 
 
 
