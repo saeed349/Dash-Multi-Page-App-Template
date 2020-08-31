57b11e7a3599ca61a6d5d5b1d4c31f833377bc18
Saeed Rahman <saeed349>
2020-08-30 21:03:27 -0400

    update: Google cloud

--- deploy-to-instance.sh
+++ deploy-to-instance.sh
@@ -2,4 +2,5 @@
 ADDRESS=$1
 USERNAME=$2
 
+apt-get update && apt-get install -y ssh
 ssh -i build.pem $USERNAME@$ADDRESS "mkdir -p /git && git clone $GIT_REPO /git/ats; cd /git/ats && git pull && docker-compose up --build"
