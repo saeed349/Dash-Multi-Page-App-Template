2d12a6cc03fcdd1caadf0f6fdd0eb4457a22ba7b
Saeed Rahman <saeed349>
2020-08-30 21:21:13 -0400

    FIX: Cloud

--- deploy-to-instance.sh
+++ deploy-to-instance.sh
@@ -3,4 +3,4 @@ ADDRESS=$1
 USERNAME=$2
 
 apt-get update && apt-get install -y ssh
-ssh -i build.pem $USERNAME@$ADDRESS "mkdir -p /git && git clone $GIT_REPO /git/ats; cd /git/ats && git pull && docker-compose up --build"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i build.pem $USERNAME@$ADDRESS "mkdir -p /git && git clone $GIT_REPO /git/ats; cd /git/ats && git pull && docker-compose up --build"
