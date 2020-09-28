2254b6ae1995bc381a71b044c4f1325873b34a08
Saeed Rahman <saeed349>
2020-09-27 23:59:32 -0400

    dag

--- Storage/airflow/dags/dag_d_zerodha.py
+++ Storage/airflow/dags/dag_d_zerodha.py
@@ -16,10 +16,10 @@ DAG_DEFAULT_ARGS={
 
 with DAG('zerodha_d', start_date=datetime(2020,1,1), schedule_interval="5 0 * * *",default_args=DAG_DEFAULT_ARGS, catchup=False) as dag:  #timedelta(days=1)
     
-    update_secmaster_db = PythonOperator(task_id="update_secmaster_db",python_callable=zerodha_historical.main,op_kwargs={'freq':'d','initial_start_date':datetime(2015,1,1)})
+    update_secmaster_db = PythonOperator(task_id="update_secmaster_db",python_callable=zerodha_historical.main,op_kwargs={'freq':'d','initial_start_date':datetime(2016,1,1)})
 
     update_ind_db = BashOperator(
-        bash_command="""python /usr/local/airflow/dags/q_pack/q_run/run_BT.py --fromdate='2015-1-1' --timeframe='d' --load_symbol='True' --univer='Indian Equity'""",
+        bash_command="""python /usr/local/airflow/dags/q_pack/q_run/run_BT.py --fromdate='2016-1-1' --timeframe='d' --load_symbol='True' --univer='Indian Equity'""",
         task_id='update_ind_db')
 
     update_secmaster_db >> update_ind_db
