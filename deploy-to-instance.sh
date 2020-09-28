d6ef28ff5689cae10b09ab7736dc30948d0b997d
Saeed Rahman <saeed349>
2020-09-27 23:30:29 -0400

    adding the deployement code for cloud compute

--- deploy-to-instance.sh
+++ deploy-to-instance.sh
@@ -6,4 +6,4 @@ GIT_REPO=$3
 
 test -f build.pem && chmod 600 build.pem
 apt-get update && apt-get install -y ssh
-ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i build.pem $USERNAME@$ADDRESS "mkdir -p /git && git clone $GIT_REPO /git/ats; cd /git/ats && git pull && docker-compose up --build -d"
+ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i build.pem $USERNAME@$ADDRESS "mkdir -p /git && git clone $GIT_REPO /git/ats; cd /git/ats && git reset --hard && git pull && docker-compose up --build -d"
