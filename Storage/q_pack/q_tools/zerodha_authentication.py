ebf5f715c69a11dd05f3212ea1246dbf54eaa30b
Saeed Rahman <saeed349>
2020-10-04 00:29:13 -0400

    cloud update

--- Storage/q_pack/q_tools/zerodha_authentication.py
+++ Storage/q_pack/q_tools/zerodha_authentication.py
@@ -1,15 +1,24 @@
 from kiteconnect import KiteConnect
 import requests
 import re
+import time
 
 def zerodha_authentication(api_key,api_secret,userid,password,pin):
     kite=KiteConnect(api_key,api_secret)
     login_url=kite.login_url()
     url = "http://etl-image:8060/zerodha_api"
     payload = {'userid': userid, 'password': password,'pin':pin,'url':login_url}
-    r = requests.get(url=url,params=payload)
     regex_token = re.compile("request_token=(.*)&action=login")
-    access_token=regex_token.search(r.text).group(1)
+    access_token = None
+    while access_token is None:
+        try:
+            r = requests.get(url=url,params=payload)
+            access_token=regex_token.search(r.text).group(1)
+        except:
+            time.sleep(3)
+            pass    
     data = kite.generate_session(access_token, api_secret=api_secret)
     kite.set_access_token(data["access_token"])
     return kite
+
+
